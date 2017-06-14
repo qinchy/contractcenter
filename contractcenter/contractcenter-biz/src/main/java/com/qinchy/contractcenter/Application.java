e78a3aa7d164ad4fbc2491d1bafc3134783e0367
qinchy <qinchy@msn.com>
2017-06-14 11:06:26 +0800

    修改包结构名称

--- contractcenter/contractcenter-biz/src/main/java/com/qinchy/contractcenter/Application.java
+++ contractcenter/contractcenter-biz/src/main/java/com/qinchy/contractcenter/Application.java
@@ -1,4 +1,4 @@
-package com.wwwarehouse.contractcenter;
+package com.qinchy.contractcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
